e22df51ae34ee99f6747c4980a82604a205217f9
a <a>
2020-06-18 15:13:35 -0500

    initial commit

---
+++ src/main/java/com/scotiabank/config/ScotiabankConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.scotiabank.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ScotiabankConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotiabankConfigServiceApplication.class, args);
+	}
+
+}
